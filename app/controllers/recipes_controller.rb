7ef5c10f00d5aba5bb5d09de8390e2de0abcbad7
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-08-01 14:12:58 +0300

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,18 @@
 class RecipesController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_recipe, only: %i[show edit update destroy]
+  before_action :authenticate_user!
+  before_action :set_recipe, only: %i[show edit update destroy]
 
-    def index
-        @recipes = Recipe.all
-    end
+  def index
+    @recipes = Recipe.all
+  end
 
-    def show
-        @recipe = Recipe.find(params[:id])
-    end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 
-    private
+  private
 
-    def set_recipe
-        @post = Post.find(params[:id])
-      end
+  def set_recipe
+    @post = Post.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_user, only: %i[show edit update destroy]
 
-    def index
-        @users = User.all
-    end
+  def index
+    @users = User.all
+  end
 
-    def show
-        @user = User.find(params[:id])
-    end
+  def show
+    @user = User.find(params[:id])
+  end
 
-    private
+  private
 
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
