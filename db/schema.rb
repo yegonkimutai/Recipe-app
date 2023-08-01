ce95f5b7408451905d8058f9f6b27c95335c1086
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-08-01 14:00:33 +0000

    Add migrations and models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_01_082344) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_01_135830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.decimal "price"
+    t.integer "quantity"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -24,6 +35,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_082344) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
+  create_table "recipes_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "food_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_recipes_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipes_foods_on_recipe_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,5 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_082344) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
   add_foreign_key "recipes", "users"
+  add_foreign_key "recipes_foods", "foods"
+  add_foreign_key "recipes_foods", "recipes"
 end
