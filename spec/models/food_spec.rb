35bfb59eab6d0ce1adf59f36ce44c3312b94640b
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-08-03 17:49:54 +0000

    Add tests for Food model

---
+++ spec/models/food_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  subject do
+    Food.new(name: 'Chicken',
+             quantity: 2,
+             measurement_unit: 'kg',
+             price: 45,
+             user_id: 1)
+  end
+
+  before { subject.save }
+
+  it 'should have a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a price' do
+    subject.price = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a number as quantity' do
+    subject.quantity = 'abc'
+    expect(subject).to_not be_valid
+  end
+end
